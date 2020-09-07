b83cc2232588ce565f11c1f49207c10f273252d9
Mikita Melnikau <usesa@yandex.com>
2020-09-07 19:23:26 +0300

    base structure

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app-bar
+    dark
+    app
+    flat
+    color="pink"
+    >
+
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+
+  data: () => ({
+  })
+}
+</script>
